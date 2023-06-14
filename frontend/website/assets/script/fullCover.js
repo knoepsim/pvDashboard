b6138706f21e41225bb01b284a30a1aa24d6fa36
Simon <simon.info.jc@gmail.com>
2023-06-14 22:06:10 +0200

    add error handling for case when no openweather data
    loading cover did stay, when couldnt get data from openweather. now it will disappear and show, that there is no data

--- frontend/website/assets/script/fullCover.js
+++ frontend/website/assets/script/fullCover.js
@@ -4,10 +4,12 @@ checkDB().then((dbStatus) => {
       dbStatus === "OK" &&
       chart1 !== undefined &&
       chart10 !== undefined &&
-      chart11 !== undefined &&
-      chart12 !== undefined &&
-      $("#updateTime").html != "" &&
-      $("#batteryPercent").html != "⌛ %"
+      (chart11 !== undefined ||
+        $("#chart11error").html() == "Keine Daten verfügbar.") &&
+      (chart12 !== undefined ||
+        $("#chart12error").html() == "Keine Daten verfügbar.") &&
+      $("#updateTime").html() != "" &&
+      $("#batteryPercent").html() != "⌛ %"
     ) {
       clearInterval(checkForCoverRemove);
       setTimeout(() => {
@@ -22,13 +24,20 @@ checkDB().then((dbStatus) => {
     if ($("#batteryPercent").html != "⌛ %") {
       $("#cover2").html("✅");
     }
+    if (chart11 !== undefined) {
+      $("#cover3").html("✅");
+    } else if ($("#chart11error").html() == "Keine Daten verfügbar.") {
+      $("#cover3").html("❌");
+    }
     if (
       chart1 !== undefined &&
       chart10 !== undefined &&
-      chart11 !== undefined &&
-      chart12 !== undefined
+      (chart11 !== undefined ||
+        $("#chart11error").html() == "Keine Daten verfügbar.") &&
+      (chart12 !== undefined ||
+        $("#chart12error").html() == "Keine Daten verfügbar.") 
     ) {
-      $("#cover3").html("✅");
+      $("#cover4").html("✅");
     }
   }, 100);
 });
